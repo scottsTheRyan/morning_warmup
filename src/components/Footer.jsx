f415621d2fe7ed980404f3fb3464351f476c0b30
thedarkness <thedarkness@thedarknesss-MacBook-Pro.local>
2023-03-20 11:08:38 -0700

    Morning speed code complete - awe yeahgit add .

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Footer(){
+    return(
+        <footer>
+            <p>Howdy ⓒ {new Date().getFullYear()}</p>
+        </footer>
+    );
+}
+
+export default Footer;
